0e24cbe3b86013dbc09fc7b4e94430fc1159f685
corvinyy <cviny072@gmail.com>
2025-10-16 15:33:27 -0300

    Feat: Animação SlideUp para os botões da página Início

--- js/script.js
+++ js/script.js
@@ -19,11 +19,32 @@ function menuLateral() {
 
 /* (INÍCIO) - Animação do sub texto de mercados */
 document.addEventListener("DOMContentLoaded", function() {
-    const items = document.querySelectorAll(".apt-sub-lista", ".apt-botao");
+    const items = document.querySelectorAll(".apt-sub-lista");
     
     items.forEach((item, index) => {
         setTimeout(() => {
             item.classList.add("animate");
         }, index * 1000); 
     });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".apt-botao a");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                buttons.forEach((btn, index) => {
+                    setTimeout(() => {
+                        btn.classList.add("animate");
+                    }, index * 600); 
+                });
+                observer.disconnect(); 
+            }
+        });
+    }, { threshold: 0.3 }); // ativa quando 30% estiver visível
+
+    if (buttons.length > 0) {
+        observer.observe(buttons[0].parentElement);
+    }
 });
